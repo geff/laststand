fe1ace0029a583ac6820a2d6e784457482d5c652
Troll <trolldu26@gmail.com>
2013-03-23 21:32:20 +0100

    The HUD should now be able to display a capacity and the cooldwoan overlay over it... when the playId and all that stuff will be implemented !(debug code instead, right now)

--- Unity/Assets/Scripts/Hud.cs
+++ Unity/Assets/Scripts/Hud.cs
@@ -6,17 +6,21 @@ public class Hud : MonoBehaviour
 {
 	
 	public void Start() {
-		foreach (var capa in GameSingleton.Instance.context.playerList[GameSingleton.Instance.context.player.playerID].playerTank.GetComponents<BaseCapacity>()) {
-			GUISkill gsk = new GUISkill();
-			gsk.setLinkedCapa(capa);
-			skills.Add(gsk);
-		}
+		//foreach (BaseCapacity capa in GameSingleton.Instance.context.playerList[GameSingleton.Instance.context.player.playerID].playerTank.GetComponents<BaseCapacity>()) {
+		//	GUISkill gsk = new GUISkill();
+		//	gsk.setLinkedCapa(capa);
+		//	skills.Add(gsk);
+		//}
+		// debug code :
+		BaseCapacity dash = new Dash();
+		GUISkill gsk2 = new GUISkill();
+		gsk2.setLinkedCapa(dash);
+		skills.Add(gsk2);
 	}
 
 	private IList<GUISkill> skills = new List<GUISkill>();
 	
 	void OnGUI () {
-		Debug.Log("There is " + skills.Count);
 		foreach(GUISkill skill in skills) {
 			skill.display();
 		}
