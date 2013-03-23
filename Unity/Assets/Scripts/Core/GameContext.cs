6343b2afb4425631b2737260e53cb0e986342ebb
Troll <trolldu26@gmail.com>
2013-03-23 22:33:57 +0100

    HUD update + casts fix

--- Unity/Assets/Scripts/Core/GameContext.cs
+++ Unity/Assets/Scripts/Core/GameContext.cs
@@ -35,7 +35,7 @@ public class GameContext : MonoBehaviour
 	void Awake()
 	{
         // Initialize the master server
-#if true
+#if false
         MasterServer.ipAddress = GameContext.MASTER_SERVER_IP_ADDRESS;
         MasterServer.port = GameContext.MASTER_SERVER_PORT;
         Network.natFacilitatorIP = GameContext.MASTER_SERVER_IP_ADDRESS;
