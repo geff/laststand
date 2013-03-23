61badc2390d26299450fd0a1e2487b7e5b59e977
Troll <trolldu26@gmail.com>
2013-03-23 21:34:37 +0100

    And now some code to locate appropriately (nearly) the capacities in the HUD

--- Unity/Assets/Scripts/Hud.cs
+++ Unity/Assets/Scripts/Hud.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 
 public class Hud : MonoBehaviour
 {
-	
+	public int capacityWidth = 100; // The width of the capacity button/texture2D to be displayed in the HUD
 	public void Start() {
+		int i = 0;
 		//foreach (BaseCapacity capa in GameSingleton.Instance.context.playerList[GameSingleton.Instance.context.player.playerID].playerTank.GetComponents<BaseCapacity>()) {
 		//	GUISkill gsk = new GUISkill();
+		//	gsk.positionX = i * capacityWidth + 10;
 		//	gsk.setLinkedCapa(capa);
 		//	skills.Add(gsk);
+		//	i++;
 		//}
 		// debug code :
 		BaseCapacity dash = new Dash();
