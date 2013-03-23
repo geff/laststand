194b4662f51a21575d2bfdead44c84d081d4a12c
Antoine Vugliano <antoine.vugliano@free.fr>
2013-03-23 22:37:18 +0100

    again (oubli de fichiers avant)

---
+++ Unity/Assets/Scripts/Weapons/Shotgun.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shotgun : BaseCapacity
+{
+
+    public override void ApplyCapacity()
+    {
+        base.ApplyCapacity();
+
+    }
+}
