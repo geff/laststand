5b8d2fd89b53d76764627e1020a0c93473fb2afa
Troll <trolldu26@gmail.com>
2013-03-23 22:20:48 +0100

    Uncommenting the actual code that should be executed

--- Unity/Assets/Scripts/Hud.cs
+++ Unity/Assets/Scripts/Hud.cs
@@ -4,21 +4,16 @@ using System.Collections.Generic;
 
 public class Hud : MonoBehaviour
 {
-	public int capacityWidth = 100; // The width of the capacity button/texture2D to be displayed in the HUD
+	public int capacityWidth = 100; // The width of the capacity button/texture2D to be displayed in the HUD	
 	public void Start() {
 		int i = 0;
-		//foreach (BaseCapacity capa in GameSingleton.Instance.context.playerList[GameSingleton.Instance.context.player.playerID].playerTank.GetComponents<BaseCapacity>()) {
-		//	GUISkill gsk = new GUISkill();
-		//	gsk.positionX = i * capacityWidth + 10;
-		//	gsk.setLinkedCapa(capa);
-		//	skills.Add(gsk);
-		//	i++;
-		//}
-		// debug code :
-		BaseCapacity dash = new Dash();
-		GUISkill gsk2 = new GUISkill();
-		gsk2.setLinkedCapa(dash);
-		skills.Add(gsk2);
+		foreach (BaseCapacity capa in GameSingleton.Instance.context.player.playerTank.GetComponents<BaseCapacity>()) {
+			GUISkill gsk = new GUISkill();
+			gsk.positionX = i * capacityWidth + 10;
+			gsk.setLinkedCapa(capa);
+			skills.Add(gsk);
+			i++;
+		}
 	}
 
 	private IList<GUISkill> skills = new List<GUISkill>();
