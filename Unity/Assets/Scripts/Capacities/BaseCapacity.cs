0a7807a75e2d0583021ef09ab6099f8abbcc577d
unknown <dev.geff@gmail.com>
2013-03-24 09:52:17 +0100

    Camera

--- Unity/Assets/Scripts/Capacities/BaseCapacity.cs
+++ Unity/Assets/Scripts/Capacities/BaseCapacity.cs
@@ -4,27 +4,27 @@ using System;
 
 public abstract class BaseCapacity : MonoBehaviour
 {
-    /// <summary>
-    /// Cooldown en secondes
-    /// </summary>
-    public float CoolDown;
-    public float LastActivity;
-    public string Key;
-    public string CapacityName;
+	/// <summary>
+	/// Cooldown en secondes
+	/// </summary>
+	public float CoolDown;
+	public float LastActivity;
+	public string Key;
+	public string CapacityName;
 	public Texture2D GUIPicture;
 
-    //[HideInInspector]
-    public VehicleController ParentVehicle;
+	//[HideInInspector]
+	public VehicleController ParentVehicle;
 
-    public void Start()
-    {
-        this.ParentVehicle = this.transform.gameObject.GetComponentInChildren<VehicleController>();
-        this.LastActivity = - this.CoolDown;
-    }
+	public void Start()
+	{
+		this.ParentVehicle = this.transform.gameObject.GetComponentInChildren<VehicleController>();
+		this.LastActivity = - this.CoolDown;
+	}
 
-    public virtual void ApplyCapacity()
-    {
-        this.LastActivity = Time.time;
-        Debug.Log(this.CapacityName + " " + this.LastActivity.ToString());
-    }
+	public virtual void ApplyCapacity()
+	{
+		this.LastActivity = Time.time;
+		Debug.Log(this.CapacityName + " " + this.LastActivity.ToString());
+	}
 }
