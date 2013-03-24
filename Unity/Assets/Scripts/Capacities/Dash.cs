1915d99bd71b4eac1ba0e0eb126a3f18fa6ac2c7
unknown <dev.geff@gmail.com>
2013-03-24 12:46:41 +0100

    Networkview on turret

--- Unity/Assets/Scripts/Capacities/Dash.cs
+++ Unity/Assets/Scripts/Capacities/Dash.cs
@@ -8,13 +8,13 @@ public class Dash : BaseCapacity
         this.CapacityName = "Dash";
         this.Key = "&";
         this.CoolDown = 3f;
+        this.AnimationName = "";
     }
 
     public override void ApplyCapacity()
     {
         base.ApplyCapacity();
         //TODO : code du Dash
-        this.ParentVehicle.Life += 50;
 		this.ParentVehicle.rigidbody.AddForce(500f, 0f, 0f, ForceMode.Force);
     }
 }
